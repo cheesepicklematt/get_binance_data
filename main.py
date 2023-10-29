6091a445c10e821b20dd907a5b66f172931225de
matthew milne <matt_milne@outlook.com>
2023-10-29 16:46:47 +1100

    a

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from get_binance_data.config import cred
 from get_binance_data.src.get_data_utils import xHourVolume
 
 
-vd = xHourVolume(hours=12,top_pct=0.5,save_path='')
+vd = xHourVolume(hours=12,top_pct=0.5)
 volume_data = vd.run()
 
 
+
+[x for x in volume_data.columns if x.find('time')>-1]
